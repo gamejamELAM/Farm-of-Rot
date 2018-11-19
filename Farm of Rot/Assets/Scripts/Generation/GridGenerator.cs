3edb4f5eebdef7ab764a17a564f691ccb021c141
Justinas Grigas <jgrigas@elam.co.uk>
2018-11-19 15:57:21 +0000

    wip

---
+++ Farm of Rot/Assets/Scripts/Generation/GridGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Generation
+{
+	public class GridGenerator : MonoBehaviour
+	{
+		[SerializeField] private Tilemap[] _tileMaps;
+		[SerializeField] private TileBase[] _tiles;
+
+		private void Start()
+		{
+			GenerateCell(Vector2Int.zero, 7);
+		}
+
+		private void GenerateCell(Vector2Int offset, int width)
+		{
+			var positionArray = new List<Vector3Int>();
+			var tileList = new List<TileBase>();
+
+			for (var y = offset.y; y < offset.y + width; y++)
+			{
+				for (var x = offset.x; x < offset.x + width; x++)
+				{
+					var tilePosition = new Vector3Int(x, y, 0);
+					var isLight = (y * width + x) % 2;
+
+					positionArray.Add(tilePosition);
+
+					tileList.Add(_tiles[isLight == 0 ? 0 : 1]);
+				}
+			}
+
+			_tileMaps[0].SetTiles(positionArray.ToArray(), tileList.ToArray());
+		}
+	}
+}
--- Farm of Rot/Assets/Scripts/Player/PlayerMovement.cs
+++ Farm of Rot/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ namespace Player
 	{
 		[SerializeField] private float _maxSpeed;
 		[SerializeField] private float _movementSpeed;
+		[SerializeField, Range(0, 1)] private float _stopSpeed;
 
 		private Rigidbody2D _rigidBody;
 		private CapsuleCollider2D _capsuleCollider;
@@ -21,10 +22,21 @@ namespace Player
 			var horizontal = Input.GetAxisRaw("Horizontal");
 			var vertical = Input.GetAxisRaw("Vertical");
 
-			if (Mathf.Abs(_rigidBody.velocity.x) + Mathf.Abs(_rigidBody.velocity.y) <= _maxSpeed)
+			var totalVelocity = Mathf.Abs(_rigidBody.velocity.x) + Mathf.Abs(_rigidBody.velocity.y);
+			var totalInput = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+
+			print(totalInput);
+
+			// Move the player.
+			if (totalInput >= 0 && totalVelocity <= _maxSpeed)
 			{
 				var newForce = new Vector3(horizontal, vertical, 0).normalized * _movementSpeed;
 				_rigidBody.AddForce(newForce);
+			}
+			// Slow the player down, if he isn't moving.
+			else if (totalInput <= 0)
+			{
+				_rigidBody.velocity = Vector2.Lerp(_rigidBody.velocity, Vector2.zero, _stopSpeed);
 			}
 		}
 	}
